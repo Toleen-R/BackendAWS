67c8dfea8a23100460811a7fa486c21a8448e653
David Heidari <davidheidari2@gmail.com>
2024-11-01 14:09:47 +0100

    updated repository

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import passport from "passport";
 import session from "express-session";
 import dotenv from "dotenv";
 import cors from "cors";
-import './authStrategies/localStrategy'
+import "./authStrategies/localStrategy";
 
 //routes
 import { campaignRoutes } from "./routes/campaign";
@@ -11,8 +11,14 @@ import authRouter from "./routes/auth";
 import { router as userRouter } from "./routes/user";
 
 const app = express();
+
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://main.d5od8j2t64g0j.amplifyapp.com/"]
+    : ["http://localhost:5173"];
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -34,15 +40,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 // app.use(cors())
 
-
 // ROUTES
 app.use(express.json());
 app.use("/campaign", campaignRoutes);
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+// const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
-app.listen(SERVER_PORT, () => {
-  console.log("Server started on: " + SERVER_PORT);
-});
+// app.listen(SERVER_PORT, () => {
+//   console.log("Server started on: " + SERVER_PORT);
+// });
+
+if(process.env.VERCEL !== "1"){
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`);)
+}
+
+export default app;
